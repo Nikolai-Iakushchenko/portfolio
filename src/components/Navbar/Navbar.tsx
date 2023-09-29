e1ae95879045d815337f1c262fa5e7d3f20a9ae2
Nick <nick22783@gmail.com>
2023-09-29 16:38:50 +0300

    styling

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+import classes from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={classes.navContainer}>
+      <div className={classes.logo}>
+        <Link href="/">Nik&apos;s Portfolio</Link>
+      </div>
+      <ul className={classes.navList}>
+        <li className={classes.navListItem}>
+          <Link href="/about">About</Link>
+        </li>
+        <li className={classes.navListItem}>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li className={classes.navListItem}>
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li className={classes.navListItem}>
+          <Link href="/resume">Resume</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
