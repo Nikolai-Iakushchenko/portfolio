9cbdff52926c60351e56c1693a15d9f8b1a7bc33
Nick <nick22783@gmail.com>
2023-11-22 18:31:45 +0300

    add Online store project + styling for project cards

--- app/projects/_project-card/project-card.tsx
+++ app/projects/_project-card/project-card.tsx
@@ -10,15 +10,12 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
   return (
     <div className={classes.projectCard}>
       <div className={classes.projectHeader}>
-        <i className="fa-regular fa-folder-open folder-icon"></i>
-        <div className={classes.smallIcons}>
-          <a href={project.gitHubLink}>
-            <i className="fa-brands fa-github"></i>
-          </a>
-        </div>
       </div>
       <h3>{project.title}</h3>
       <p>{project.description}</p>
+      <p className={classes.githublink}> <a href={project.gitHubLink}>
+               {project.gitHubLink}
+      </a></p>
     </div>
   );
 };
--- app/projects/projects-data.ts
+++ app/projects/projects-data.ts
@@ -3,42 +3,48 @@ export interface ProjectData {
   title: string;
   description: string;
   gitHubLink: string;
+  imgLink: string
 }
 
 export const projectsData: ProjectData[] = [
   {
     id: 1,
-    title: "Todo List App",
+    title: "Online Store",
     description:
-      "A simple Todo List App built with JavaScript. All datas are stored in localstorage. It helps users check list out their plans and tick as they do them.",
-    gitHubLink: "https://github.com/olawanlejoel/Todo-List-App",
+      "React, Redux, Typescript, CSS Modules",
+    gitHubLink: "https://github.com/nick722/clothing-store",
+    imgLink: "@/public/images/clothing-store-screenshot.avif"
   },
   {
     id: 2,
     title: "Books Library App",
     description:
-      "A simple Book Library App built with JavaScript. It helps readers have a good list of books they are either currently reading or have finished reading.",
-    gitHubLink: "https://github.com/olawanlejoel/Book-Library",
+      "Coming soon",
+    gitHubLink: "",
+    imgLink: ""
   },
   {
     id: 3,
     title: "Quotes Generator",
     description:
-      "Helps you generate quotes from about 1600 quotes written by different authors . Quotes are automatically copied to your clipboards.",
-    gitHubLink: "https://github.com/olawanlejoel/random-quote-generator",
+      "Coming soon",
+    gitHubLink: "",
+    imgLink: ""
   },
   {
     id: 4,
     title: "Password Generator",
     description:
-      "Helps you generates random passwords, you can select what you want your password to entail and also you can copy generated password to clipboard.",
-    gitHubLink: "https://github.com/olawanlejoel/Password-Generator",
+      "Coming soon",
+    gitHubLink: "",
+    imgLink: ""
   },
   {
     id: 5,
     title: "Twitter UI Clone",
     description:
-      "Simple Twitter UI clone built with TailwindCSS and Vue Js. This covers only the homepage of Twitter UI. This is cool to get started with TailwindCSS as it helps understand basic concepts.",
-    gitHubLink: "https://github.com/olawanlejoel/TwitterUI-clone",
+      "Coming soon",
+    gitHubLink: "",
+    imgLink: ""
   },
 ];
