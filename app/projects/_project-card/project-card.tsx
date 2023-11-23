0f0db410edab68e692df0d9b72bc1e23ef805047
Nick <nick22783@gmail.com>
2023-11-23 12:07:43 +0300

    add portfolio project

--- app/projects/_project-card/project-card.tsx
+++ app/projects/_project-card/project-card.tsx
@@ -10,12 +10,25 @@ interface ProjectCardProps {
 const ProjectCard = ({ project }: ProjectCardProps) => {
   return (
     <div className={classes.projectCard}>
-      {project.imgLink && <a target="_blank" href={project.hostingLink}><Image src={project.imgLink} alt={project.title} width={240} height={150}/></a>}
+      {project.imgPath && (
+        <div className={classes.imageContainer}>
+          <a target="_blank" href={project.hostingLink}>
+            <Image
+              src={project.imgPath}
+              alt={project.title}
+              width={240}
+              height={150}
+            />
+          </a>
+        </div>
+      )}
       <h3>{project.title}</h3>
       <p>{project.description}</p>
-      <p className={classes.githublink}> <a target="_blank" href={project.gitHubLink}>
-               {project.gitHubLink}
-      </a></p>
+      <p className={classes.githublink}>
+        <a target="_blank" href={project.gitHubLink}>
+          {project.gitHubLink}
+        </a>
+      </p>
     </div>
   );
 };
--- app/projects/projects-data.ts
+++ app/projects/projects-data.ts
@@ -3,54 +3,49 @@ export interface ProjectData {
   title: string;
   description: string;
   gitHubLink: string;
-  hostingLink: string
-  imgLink: string
+  hostingLink: string;
+  imgPath: string;
 }
 
 export const projectsData: ProjectData[] = [
   {
     id: 1,
     title: "Online Store",
-    description:
-      "React, Redux, Typescript, CSS Modules",
+    description: "React, Redux, Typescript, CSS Modules",
     gitHubLink: "https://github.com/nick722/clothing-store",
     hostingLink: "https://niks-clothing-store.netlify.app",
-    imgLink: "/images/clothing-store-screenshot.avif"
+    imgPath: "/images/clothing-store-screenshot.avif",
   },
   {
     id: 2,
-    title: "Books Library App",
-    description:
-      "Coming soon",
-    gitHubLink: "",
-    hostingLink: "",
-    imgLink: ""
+    title: "Personal Portfolio",
+    description: "Next.js, React, Typescript",
+    gitHubLink: "https://github.com/nick722/portfolio",
+    hostingLink: "https://nikolai-iakushchenko.netlify.app/",
+    imgPath: "/images/portfolio-screenshot.avif",
   },
   {
     id: 3,
     title: "Quotes Generator",
-    description:
-      "Coming soon",
+    description: "Coming soon",
     gitHubLink: "",
     hostingLink: "",
-    imgLink: ""
+    imgPath: "",
   },
   {
     id: 4,
     title: "Password Generator",
-    description:
-      "Coming soon",
+    description: "Coming soon",
     gitHubLink: "",
     hostingLink: "",
-    imgLink: ""
+    imgPath: "",
   },
   {
     id: 5,
     title: "Twitter UI Clone",
-    description:
-      "Coming soon",
+    description: "Coming soon",
     gitHubLink: "",
     hostingLink: "",
-    imgLink: ""
+    imgPath: "",
   },
 ];
