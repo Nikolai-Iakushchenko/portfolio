784f58d94335848fb782e85a70f60a76c3a2c9e6
Nick <nick22783@gmail.com>
2023-12-18 12:19:57 +0300

    wrap project's heading and description into the link

--- app/projects/_project-card/project-card.tsx
+++ app/projects/_project-card/project-card.tsx
@@ -10,20 +10,20 @@ interface ProjectCardProps {
 const ProjectCard = ({ project }: ProjectCardProps) => {
   return (
     <div className={classes.projectCard}>
-      {project.imgPath && (
-        <div className={classes.imageContainer}>
-          <a target="_blank" href={project.hostingLink}>
+      <a target="_blank" href={project.hostingLink}>
+        {project.imgPath && (
+          <div className={classes.imageContainer}>
             <Image
               src={project.imgPath}
               alt={project.title}
               width={240}
               height={150}
             />
-          </a>
-        </div>
-      )}
-      <h3>{project.title}</h3>
-      <p>{project.description}</p>
+          </div>
+        )}
+        <h3>{project.title}</h3>
+        <p>{project.description}</p>
+      </a>
       <p className={classes.githublink}>
         <a target="_blank" href={project.gitHubLink}>
           {project.gitHubLink}
